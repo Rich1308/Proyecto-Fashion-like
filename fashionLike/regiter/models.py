e1d68dcd58f1424c5afaf20dde66ee7440e38043
rich1308 <rarenas94@yahoo.es>
2023-01-29 12:53:08 -0500

    la base de datos se conecta con el backend y guarda los datosde los usuarios

--- fashionLike/regiter/models.py
+++ fashionLike/regiter/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db import models as m
+from django.contrib.auth.models import User
+
+class adduser:
+    def __init__(self,user,first_N,last_N,email,passwd):
+        self.U = User.objects.create_user(username=user,first_name=first_N,last_name=last_N,email=email,password=passwd)
+        self.U.save()
+
+    
+
 
 
